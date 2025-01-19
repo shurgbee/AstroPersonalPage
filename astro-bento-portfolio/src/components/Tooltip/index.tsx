9c09ec18ff19839e30884c15a96e4b6fac0dbd8d
Shurgbee <81319757+shurgbee@users.noreply.github.com>
2025-01-18 22:58:12 -0600

    Altered Links, Changed Now Card, and Added flippable cards and image components

--- astro-bento-portfolio/src/components/Tooltip/index.tsx
+++ astro-bento-portfolio/src/components/Tooltip/index.tsx
@@ -8,38 +8,32 @@ function Tooltip(props: Props) {
   const [isVisible, setIsVisible] = createSignal(false);
   const [clickCount, setClickCount] = createSignal(0);
 
-  const messages = [
-    "Hi there!",
-    "Clicked again?",
-    "Still here?",
-    "Persistent, aren't you?",
-    "What's up?",
-    "Again? Really?",
-    "You're curious!",
-    "Not cool!",
-    "Give it a break!",
-    "That's annoying!",
-    "Hands off!",
-    "No more clicks!",
-    "Seriously?!",
-    "Ouch! That hurts!",
-    "You're persistent!",
-    "Why the curiosity?",
-    "I'm getting tired!",
-    "I'm bored!",
-    "Enough's enough!",
-    "Find another hobby!",
-    "Stop, please!",
-    "Okay, last one!",
-    "That's it, I'm done!",
-  ];
+const messages = [
+    "The first computer programmer was Ada Lovelace in the 1840s.",
+    "The first computer virus was created in 1986 and called Brain.",
+    "The term 'debugging' comes from removing a moth from a computer.",
+    "The first electronic computer, ENIAC, weighed over 27 tons.",
+    "Alan Turing is considered the father of computer science.",
+    "Python is named after the British comedy group Monty Python.",
+    "The first hard drive could store just 5MB of data.",
+    "The first email was sent in 1971 by Ray Tomlinson.",
+    "The first website is still online and was created in 1991 by Tim Berners-Lee.",
+    "Moore's Law predicts that computer processing power doubles every two years.",
+    "The first 1GB hard drive was introduced in 1980 and weighed 550 pounds.",
+    "The QWERTY keyboard layout was designed in 1873 for typewriters.",
+    "The term 'cookie' in computing comes from 'magic cookies' in Unix programming.",
+    "The first smartphone, IBM Simon, was released in 1994.",
+    "ASCII stands for 'American Standard Code for Information Interchange.'",
+    "Java was originally named 'Oak' after a tree outside James Gosling's office.",
+    "HTML stands for 'HyperText Markup Language.'",
+    "The first gaming console, the Magnavox Odyssey, was released in 1972.",
+    "Linux, an open-source OS, was created by Linus Torvalds in 1991."
+];
+
 
   const currentMessage = () => {
     const count = clickCount();
-    if (count >= messages.length) {
-      return messages[messages.length - 1];
-    }
-    return messages[count];
+    return messages[count % messages.length];
   };
 
   return (
